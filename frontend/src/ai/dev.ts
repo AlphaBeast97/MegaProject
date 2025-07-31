c0eec4dac550eccc596d286f9ad70bf9c3e509ab
AlphaBeast97 <ksaad5272@gmail.com>
2025-07-31 21:13:26 +0500

    refactoring: removing unnecessary depenecies

--- frontend/src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-recipe-suggestions-from-image.ts';
-import '@/ai/flows/generate-recipe-suggestions-from-ingredients.ts';
-import '@/ai/flows/generate-recipe-title-and-description.ts';
