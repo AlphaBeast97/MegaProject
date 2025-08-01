c01d56d2c127db6ec8ce8f000ac3df94a7af4d3a
AlphaBeast97 <ksaad5272@gmail.com>
2025-08-01 18:24:33 +0500

    cloudinary image size resolve

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ app.use(
 
 app.use(clerkMiddleware());
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const PORT = ENV.PORT;
 
